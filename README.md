9b384d33afe4a045f6603f425057fab37c5a5216
nmrenyi <nmrenyi@outlook.com>
2026-01-08 16:43:59 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Rosenwald Guide Dataset
+
+## Contact
+
+For questions or feedback about this benchmark:
+
+**Ren Yi**  
+Email: [renyi1006@gmail.com](mailto:renyi1006@gmail.com)
+
---
+++ copy.sh
@@ -0,0 +1,3 @@
+# copy all the tsvs under /Users/renyi/Downloads/rosenwald/extraire-tesseract-openai/batch-gemini-image-text-production/analysed-tsv into ./rosenwald-extraction-with-gender
+mkdir -p rosenwald-extraction
+cp /Users/renyi/Downloads/rosenwald/extraire-tesseract-openai/batch-gemini-image-text-production/analysed-tsv/*.tsv rosenwald-extraction/
